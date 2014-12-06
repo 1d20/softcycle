2288f4ee3eabda59ba3ff1180934b9053a4843b9
detonavomek <detonavomek@gmail.com>
2014-12-07 00:58:21 +0200

    add auth oauth

--- source/urls.py
+++ source/urls.py
@@ -6,7 +6,9 @@ from django.conf.urls.static import static
 urlpatterns = patterns(
     '',
     url(r'^$', 'apps.core.views.index', name='index'),
+    url(r'^logout/$', 'apps.core.views.logout', name='logout'),
     url(r'^api/', include('apps.api.urls')),
+    url(r'', include('social_auth.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

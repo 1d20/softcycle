05c41393bf06ab5cc82a766b15541d7e913f7885
detonavomek <detonavomek@gmail.com>
2014-12-08 01:19:10 +0200

    add position/id/ api

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -5,5 +5,6 @@ urlpatterns = patterns(
     '',
     url(r'profile/$', views.profile),
     url(r'positions/$', views.positions),
+    url(r'positions/(?P<position_id>\d+)/$', views.position),
     url(r'stage/(?P<position_id>\d+)/$', views.stage),
 )

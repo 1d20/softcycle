d7ce1093a1467bb03fa889ae55f3783a9721d154
Alina Knysh <knushh1@gmail.com>
2014-12-08 00:23:18 +0200

    design style  frontend

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -19,7 +19,28 @@
         // Go!
         path.style.strokeDashoffset = '0';
 
+      function initAnimations() {
+        $('.animated').appear(function () {
+            var el = $(this);
+            var animation = el.data('animation');
+            var delay = el.data('delay');
+            if (delay) {
+                setTimeout(function () {
+                    el.addClass(animation);
+                    el.addClass('showing');
+                    el.removeClass('hiding');
+                }, delay);
+            } else {
+                el.addClass(animation);
+                el.addClass('showing');
+                el.removeClass('hiding');
+            }
+        }, {
+            accY: -60
+        });
 
+        initAnimations();
+      }
     }
 
     window.testInit = testInit;
